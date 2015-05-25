b13f795485b2f1242a5d7be6b6c950d0196ec4fc
aaronw3373 <aaronw3373@gmail.com>
2015-05-25 18:35:58 -0400

    adding images i hope 2

--- Gruntfile.js
+++ Gruntfile.js
@@ -313,7 +313,7 @@ module.exports = function (grunt) {
           dest: '<%= config.dist %>',
           src: [
             '*.{ico,png,txt}',
-            'images/**/*',
+            'images/{,*/}*.webp',
             '{,*/}*.html',
             'styles/fonts/{,*/}*.*'
           ]
