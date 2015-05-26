9b2cc4073cb47ff57d35460d5c08f930edc0760e
aaronw3373 <aaronw3373@gmail.com>
2015-05-26 08:42:01 -0400

    with photos?

--- Gruntfile.js
+++ Gruntfile.js
@@ -313,7 +313,7 @@ module.exports = function (grunt) {
           dest: '<%= config.dist %>',
           src: [
             '*.{ico,png,txt}',
-            'images/{,*/}*.webp',
+            'images/{,*/}*.jpg',
             '{,*/}*.html',
             'styles/fonts/{,*/}*.*'
           ]
