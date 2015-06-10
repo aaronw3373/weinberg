9245b8d888ec2957261f4997fd82e65b673072e9
aaronw3373 <aaronw3373@gmail.com>
2015-06-10 19:00:37 -0400

    same

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,71 +1,12 @@
  $(document).ready(function(){
-  // $('#home_button').on('click',function(){
-  //   $('#project_page').hide()
-  //   $('#about_page').hide()
-  //   $('#home_page').show()
-  // });
-
-  // $('#about_button').on('click',function(){
-  //   $('#project_page').hide()
-  //   $('#home_page').hide()
-  //   $('#about_page').show()
-  // });
-
-  // $('#projects_button').on('click',function(){
-  //   $('#about_page').hide()
-  //   $('#home_page').hide()
-  //   $('#ttt_page').hide()
-  //   // $('#splitboard_page').hide()
-  //   // $('#snowmachine_page').hide()
-  //   // $('#speakers_page').hide()
-  //   $('#powder_page').hide()
-  //   $('#project_page').show()
-  // });
-
-    $('#ttt_button').on('click',function(){
-      $('#playbox').hide()
-      $('#chatbox').hide()
-      $('#foot').hide()
-      $('#lfoot').hide()
-      $('#cfoot').hide()
-      // $('#splitboard_page').hide()
-      // $('#snowmachine_page').hide()
-      // $('#speakers_page').hide()
-      $('#powder_page').hide()
-      $('#ttt_page').show()
-      $('#login').show()
-    });
-
-    $('#powder_button').on('click',function(){
-      $('#ttt_page').hide()
-      // $('#splitboard_page').hide()
-      // $('#snowmachine_page').hide()
-      // $('#speakers_page').hide()
-      $('#powder_page').show()
-    });
-
-    // $('#splitboard_button').on('click',function(){
-    //   $('#ttt_page').hide()
-    //   $('#snowmachine_page').hide()
-    //   $('#speakers_page').hide()
-    //   $('#powder_page').hide()
-    //   $('#splitboard_page').show()
-    // });
-
-    // $('#snowmachine_button').on('click',function(){
-    //   $('#ttt_page').hide()
-    //   $('#speakers_page').hide()
-    //   $('#powder_page').hide()
-    //   $('#splitboard_page').hide()
-    //   $('#snowmachine_page').show()
-    // });
-
-    // $('#speakers_button').on('click',function(){
-    //   $('#ttt_page').hide()
-    //   $('#powder_page').hide()
-    //   $('#splitboard_page').hide()
-    //   $('#snowmachine_page').hide()
-    //   $('#speakers_page').show()
-    // });
+  $('#projects_button').on('click',function(){
+    $('html, body').animate({
+        scrollTop: $("projects_page_location").offset()
+    }, 2000);
+  });
+  $('#home_button').on('click',function(){
+  });
+  $('#about_button').on('click',function(){
+  });
 
  });
